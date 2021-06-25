34af7db9549bf022647c1f0f3279507d2d719294
conan <det_conan@Live.cn>
2021-06-26 01:24:01 +0900

    Finish server side

--- msg_receiver.py
+++ msg_receiver.py
@@ -1,9 +1,113 @@
-from flask import Flask
-application = Flask(__name__)
+import configparser
+import ipaddress
+import secrets
+import sqlite3
 
-@application.route("/")
+import mysql.connector
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_bcrypt import Bcrypt
+
+from queries import Queries
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+default_config = config["DEFAULT"]
+if default_config["DatabaseType"] == "MySQL":
+    cnx = mysql.connector.connect(
+        host=default_config["MysqlEndpoint"],
+        user=default_config["DatabaseUserName"],
+        password=default_config["DatabasePassword"],
+        database=default_config["DatabaseName"],
+    )
+elif default_config["DatabaseType"] == "SQLite":
+    cnx = sqlite3.connect(default_config["DatabaseName"] + ".db")
+
+else:
+    raise ValueError
+
+authorized_networks = ipaddress.IPv4Network(default_config["AuthorizedNetwork"])
+
+cursor = cnx.cursor()
+queries = Queries()
+
+cursor.execute(queries.create_authorized_users_table())
+cursor.execute(queries.create_devices_table())
+cursor.execute(queries.create_device_status_table())
+
+
+@app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
+
+@app.route("/api/device_registration", methods=["POST"])
+def device_registration():
+    info = request.json
+    user_name = info["user_name"]
+    password = info["password"]
+    cursor.execute(queries.get_user_password(user_name))
+    user_info = cursor.fetchone()
+    if user_info is None:
+        return jsonify("User not found"), 404
+    if bcrypt.check_password_hash(password, user_info[0]):
+        device_serial = info["device_serial"]
+        device_location = info["device_location"]
+        description = info["description"]
+        authorization_token = secrets.token_urlsafe(32)
+        cursor.execute(
+            queries.register_device(
+                device_serial, device_location, description, authorization_token
+            )
+        )
+        cnx.commit()
+        return jsonify({"authorization_token": authorization_token}), 200
+    else:
+        return jsonify("Invalid username/password combination"), 400
+    pass
+
+
+@app.route("/api/user_registration", methods=["POST"])
+def user_registration():
+    remote_addr = request.environ.get("HTTP_X_REAL_IP", request.remote_addr)
+    if (
+        ipaddress.ip_address(remote_addr) in authorized_networks
+        or remote_addr == "127.0.0.1"
+    ):
+        info = request.json
+        user_name = info["user_name"]
+        password = info["password"]
+        pw_hash = bcrypt.generate_password_hash(password)
+        cursor.execute(queries.register_user(user_name, pw_hash))
+        cnx.commit()
+        return jsonify(f"Created user {user_name}")
+    else:
+        return jsonify("Request from Unauthorized Network"), 400
+
+
+@app.route("/api/device_status_update")
+def device_status_update():
+    info = request.json
+    device_serial = info["device_serial"]
+    authorization_token = info["authorization_token"]
+    cursor.execute(queries.get_device_authorization_token(device_serial))
+    device_info = cursor.fetchone()
+    if device_info is None:
+        return jsonify("Device not found"), 404
+    elif authorization_token == device_info[0]:
+        metric_name = info["metric_name"]
+        metric_value = eval(info["metric_value"])
+        cursor.execute(
+            queries.log_status(device_serial, metric_name, float(metric_value))
+        )
+        cnx.commit()
+        return jsonify("Status logged"), 200
+    else:
+        return jsonify("Device serial and authorization token mismatch"), 400
+
+
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
---
+++ queries.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+
+
+class Queries:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create_authorized_users_table():
+        return (
+            "CREATE TABLE IF NOT EXISTS AUTHORIZED_USERS("
+            "ID INT AUTO_INCREMENT PRIMARY KEY,"
+            "USER_NAME VARCHAR(50) NOT NULL UNIQUE ,"
+            "PASSWORD VARCHAR(128) NOT NULL,"
+            "REGISTRATION_TIME TIMESTAMP NOT NULL )"
+        )
+
+    @staticmethod
+    def create_devices_table():
+        return (
+            "CREATE TABLE IF NOT EXISTS DEVICES("
+            "DEVICE_ID INT AUTO_INCREMENT PRIMARY KEY ,"
+            "DEVICE_SERIAL VARCHAR(30) NOT NULL UNIQUE ,"
+            "DEVICE_LOCATION VARCHAR(50),"
+            "DESCRIPTION VARCHAR(100),"
+            "AUTHORIZATION_TOKEN VARCHAR(32) NOT NULL,"
+            "REGISTRATION_TIME TIMESTAMP NOT NULL)"
+        )
+
+    @staticmethod
+    def create_device_status_table():
+        return (
+            "CREATE TABLE IF NOT EXISTS DEVICE_STATUS("
+            "STATUS_ID BIGINT AUTO_INCREMENT PRIMARY KEY ,"
+            "DEVICE_SERIAL VARCHAR(30),"
+            "METRIC_NAME VARCHAR(32) NOT NULL ,"
+            "METRIC_VALUE FLOAT NOT NULL,"
+            "LOGGING_TIME TIMESTAMP )"
+        )
+
+    @staticmethod
+    def log_status(device_serial: str, metric_name: str, metric_value: float):
+        return f"INSERT INTO DEVICE_STATUS (DEVICE_SERIAL,METRIC_NAME,METRIC_VALUE,LOGGING_TIME) VALUES ({device_serial},{metric_name},{metric_value},{datetime.now().strftime('%Y-%m-%d %H:%M:%S')})"
+
+    @staticmethod
+    def register_user(user_name: str, pw_hash: str):
+        return (
+            f"INSERT INTO AUTHORIZED_USERS (USER_NAME,PASSWORD,REGISTRATION_TIME) VALUES ({user_name},{pw_hash},"
+            f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')})"
+        )
+
+    @staticmethod
+    def get_user_password(user_name: str):
+        return f"SELECT PASSWORD FROM AUTHORIZED_USERS WHERE USER_NAME = {user_name}"
+
+    @staticmethod
+    def register_device(
+        device_serial: str,
+        device_location: str,
+        description: str,
+        authorization_token: str,
+    ):
+        return (
+            f"INSERT INTO DEVICES (DEVICE_SERIAL,DEVICE_LOCATION,DESCRIPTION,AUTHORIZATION_TOKEN,REGISTRATION_TIME)"
+            f"VALUES ({device_serial},{device_location},{description},{authorization_token},{datetime.now().strftime('%Y-%m-%d %H:%M:%S')})"
+        )
+
+    @staticmethod
+    def get_device_authorization_token(device_serial: str):
+        return f"SELECT AUTHORIZATION_TOKEN FROM DEVICES WHERE DEVICE_SERIAL = {device_serial}"
