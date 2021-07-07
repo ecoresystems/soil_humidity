6fae884db0f2ada9c64ef4f79e7bd57f9d638e23
liu-l <liu-l@klab.com>
2021-07-07 14:45:31 +0900

    update backend and front end with device script

--- device_scripts/send_status_update.py
+++ device_scripts/send_status_update.py
@@ -1,20 +1,34 @@
 import configparser
 
+import adafruit_ads1x15.ads1115 as ADS
+import board
+import busio
 import requests
+from adafruit_ads1x15.analog_in import AnalogIn
 
 
-def get_ADC_value():
-    # TODO: Acquire ADC value here
-    return "Test metric", 2.048
+def channels(channel: int):
+    return {
+        0: ADS.P0,
+        1: ADS.P1,
+        2: ADS.P2,
+        3: ADS.P3,
+    }[channel]
 
 
-def send_update():
+def get_ADC_value(channel: int = 0):
+    i2c = busio.I2C(board.SCL, board.SDA)
+    ads = ADS.ADS1115(i2c)
+    return f"Voltage/CH{channel}", AnalogIn(ads, channels(channel)).voltage
+
+
+def send_update(channel: int = 0):
     config = configparser.ConfigParser()
     config.read('config.ini')
     url = config['DEFAULT']['StatusUpdateEndPoint']
     device_serial = config['DEFAULT']['DeviceSerial']
     authorization_token = config['DEFAULT']['AuthorizationToken']
-    metric_name, metric_value = get_ADC_value()
+    metric_name, metric_value = get_ADC_value(channel)
     request_data = {
         "metric_name": metric_name,
         "metric_value": metric_value,
@@ -26,4 +40,6 @@ def send_update():
 
 
 if __name__ == "__main__":
-    send_update()
+    active_channels = [0, 1]
+    for query_channel in active_channels:
+        send_update(query_channel)
